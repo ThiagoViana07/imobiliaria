b2c5019153d088e599578a9c5ee8e0c50550b628
ThiagoViana07 <thiago_viana99@hotmail.com>
2026-05-09 10:37:38 -0300

    fix app.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
-const path = require("path");
-const fs = require("fs");
+
 
 const app = express();
 app.use(express.json());

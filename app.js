df22e6c904dda719860e872f1a2dc38b88c642a9
ThiagoViana07 <thiago_viana99@hotmail.com>
2026-05-30 08:47:14 -0300

    connect database app.js

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ import fs from 'fs';
 import path from 'path';
 import express from 'express';
 import { fileURLToPath } from 'url';
+import pool from './src/config/dbConnect.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const conexao = await pool();
+
+conexao.on('error', (erro) => {
+  console.error('erro de conexão', erro);
+});
+
+conexao.once('open', () => {
+  console.log('Conexão com o banco feita com sucesso');
+});
 
 const app = express();
 app.use(express.json());

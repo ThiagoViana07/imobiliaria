dede44d5532108a238532540c4861a3494970afc
ThiagoViana07 <thiago_viana99@hotmail.com>
2026-05-30 08:49:07 -0300

    fix: db connect

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -8,14 +8,4 @@ async function pool() {
   return mongoose.connection;
 }
 
-const conexao = await pool();
-
-conexao.on('error', (erro) => {
-  console.error('erro de conexão', erro);
-});
-
-conexao.once('open', () => {
-  console.log('Conexão com o banco feita com sucesso');
-});
-
 export default pool;
